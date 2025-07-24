dc9456dcf21d525b80e94faa5b6e6917c6768140
daramsreya83@gmail.com <your.email@example.com>
2025-07-24 12:32:12 +0530

    Assigment

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const otp=document.querySelectorAll(".code")
+let index=0;
+otp.forEach((e,index)=>{
+    e.addEventListener('input',()=>{
+        if(e.value && index<otp.length-1){
+            otp[index+1].focus();
+        }
+    })
+    e.addEventListener('keydown',(element)=>{
+        if(element.key==="Backspace" && e.value==="" && index>0){
+            otp[index-1].focus();
+        }
+    })
+})
 //your JS code here. If required.
